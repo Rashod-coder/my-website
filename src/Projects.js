186583033cf3a5a34038225aadceb1fee7cffcfd
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-07-24 23:08:21 -0700

    changes made

--- src/Projects.js
+++ src/Projects.js
@@ -6,6 +6,8 @@ const ProjectsPage = () => {
   return (
     <div style={{height: '100vh', backgroundColor: '#f4f4f4'}}>
         <h1 className='display-2'>Projects</h1>
+
+        Techcore
     </div>
     
   );
--- src/testing.py
+++
@@ -1,8 +0,0 @@
-plays = {
-    'Macbeth': 'William Shakespeare',
-    'A Streetcar Named Desire': 'Tennessee Williams',
-    'Amadeus': 'Peter Shaffer'
-}
-
-for title, author in plays.items():  
-    print(author, "wrote", title)
